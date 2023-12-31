d9672868a55bc22746f80ee94d884d28bcd5a6a9
wali55 <walisantunu@gmail.com>
2023-12-31 14:28:21 +0600

    book list

--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
-from .models import Book, BookInstance, Author
+from .models import Book, BookInstance, Author, Genre
 
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+    num_books_contain_the = Book.objects.filter(title__icontains='the').count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_contain_the': num_books_contain_the,
     }
 
     return render(request, 'index.html', context=context)
