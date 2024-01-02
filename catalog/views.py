87dad5d78cc4abac10bee99643db73a8666089d7
wali55 <walisantunu@gmail.com>
2024-01-02 20:03:58 +0600

    sessions

--- catalog/views.py
+++ catalog/views.py
@@ -9,6 +9,8 @@ def index(request):
     num_authors = Author.objects.count()
     num_genres = Genre.objects.count()
     num_books_contain_the = Book.objects.filter(title__icontains='the').count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
@@ -17,6 +19,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'num_books_contain_the': num_books_contain_the,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
